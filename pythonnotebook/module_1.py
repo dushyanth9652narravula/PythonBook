0efc9c22d705a1ac0ec83927d28558f8b64eaac9
dushyanth9652narravula <narravula.dushyanthsai@gmail.com>
2025-06-19 12:04:15 -0400

    Reloading the modules has been added

---
+++ pythonnotebook/module_1.py
@@ -0,0 +1,10 @@
+# module_1.py
+
+print('running module_1.py....')
+
+def print_values():
+	print('k1','10')
+	print('k2','apple')
+	print('k3','mango')
+	print('k4','Cheese')
+	print('k5','Banana')
